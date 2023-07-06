87486c6ee15fc34f23bc36ade93e3e626fec9a47
cauan <116805833+cauannz@users.noreply.github.com>
2023-07-05 21:19:13 -0300

    Delete Parada_19/js directory

--- Parada_19/js/script.js
+++
@@ -1,5 +0,0 @@
-function saudacao() {
-    var nome = document.getElementById('nome').value;
-    var saudacao = "Olá, " + nome + "!";
-    document.getElementById('mensagem').innerHTML = saudacao;
-}
